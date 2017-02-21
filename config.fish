2e351ab791785561c4617ce66915cb1682f9b67a
Jeff Hertzler <jeffhertzler@gmail.com>
2017-02-21 00:06:37 -0500

    allow fish to complete suggestion with C-F in vi mode

--- config.fish
+++ config.fish
@@ -107,6 +107,11 @@ function fish_right_prompt
 end
 
 fish_vi_key_bindings
+function fish_user_key_bindings
+    for mode in insert default visual
+        bind -M $mode \cf forward-char
+    end
+end
 
 if test ! -d $HOME/bin
   mkdir $HOME/bin
