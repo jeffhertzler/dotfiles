137076285417a2346db73c5b12d2014324b3ffa0
Jeff Hertzler <jeffhertzler@gmail.com>
2017-04-02 11:45:44 -0400

    don't override all the vim style key binding
    moved to fish_user_key_bindings.fish

--- config.fish
+++ config.fish
@@ -107,11 +107,6 @@ function fish_right_prompt
 end
 
 fish_vi_key_bindings
-function fish_user_key_bindings
-    for mode in insert default visual
-        bind -M $mode \cf forward-char
-    end
-end
 
 if test ! -d $HOME/bin
   mkdir $HOME/bin
