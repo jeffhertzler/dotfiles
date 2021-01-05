920c596f540d06984cd2379d9f70a5a41f041174
Jeff Hertzler <jeffhertzler@gmail.com>
2021-01-05 00:07:44 -0500

    clean slate!

--- nvim/lua/helpers.lua
+++
@@ -1,8 +0,0 @@
-function _G.dump(...)
-  local objects = vim.tbl_map(vim.inspect, {...})
-  print(unpack(objects))
-end
-
-function _G.keymap(mode, lhs, rhs, opts)
-  vim.api.nvim_set_keymap(mode, lhs, rhs, opts or {});
-end
