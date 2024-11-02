850e10d75ece23cd6b00e467254137b25d545737
Jeff Hertzler <jeffhertzler@gmail.com>
2024-11-02 15:45:53 -0400

    yazi updates

--- dot_config/nvim/lua/plugins/yazi.lua
+++ dot_config/nvim/lua/plugins/yazi.lua
@@ -17,11 +17,6 @@ return {
       "<cmd>Yazi cwd<cr>",
       desc = "Open the file manager in nvim's working directory",
     },
-    {
-      "<c-up>",
-      "<cmd>Yazi toggle<cr>",
-      desc = "Resume the last yazi session",
-    },
   },
   ---@type YaziConfig
   opts = {
