f3ccac2c81e945a960b97cab5c8e101684131a9e
Jeff Hertzler <jeffhertzler@gmail.com>
2024-11-27 14:09:23 -0500

    fix vimme db dump/restore

--- dot_vimme.zsh
+++ dot_vimme.zsh
@@ -3,19 +3,19 @@ VIMME="$HOME/dev/vimme"
 abbrev-alias -c cdv="cd $VIMME"
 
 vldbdump() {
-  ssh "${VIMME_USER}@${VIMME_SERVER} mysqldump --add-drop-database -u ${VIMME_DB_USER} -p${VIMME_DB_PASS} --databases ${VIMME_DB_NAME}" > "${1:-vimme.dump}"
+  ssh "${VIMME_USER}@${VIMME_SERVER}" mysqldump --add-drop-database -u $VIMME_DB_USER -p$VIMME_DB_PASS --databases $VIMME_DB_NAME > "${1:-vimme.dump}"
 }
 vmdbdump() {
-  ssh "${MATHBOTS_USER}@${MATHBOTS_SERVER} mysqldump --add-drop-database -u ${MATHBOTS_DB_USER} -p${MATHBOTS_DB_PASS} --databases ${MATHBOTS_DB_NAME}" > "${1:-mathbots.dump}"
+  ssh "${MATHBOTS_USER}@${MATHBOTS_SERVER}" mysqldump --add-drop-database -u $MATHBOTS_DB_USER -p$MATHBOTS_DB_PASS --databases $MATHBOTS_DB_NAME > "${1:-mathbots.dump}"
 }
 
 vldbimport() {
-  sed -i "s/`${VIMME_DB_NAME}`/`laravel`/g" "${1:-vimme.dump}"
-  mysql -h 0.0.0.0 -P 33060 -ppassword -u sail laravel < "${1:-vimme.dump}"
+  sed -i "s/\`${VIMME_DB_NAME}\`/\`laravel\`/g" "${1:-vimme.dump}"
+  mariadb -h 127.0.0.1 -P 33060 -ppassword -usail laravel < "${1:-vimme.dump}"
 }
 vmdbimport() {
-  sed -i "s/`${MATHBOTS_DB_NAME}`/`laravel`/g" "${1:-mathbots.dump}"
-  mysql -h 0.0.0.0 -P 33061 -psecret -u sail laravel < "${1:-mathbots.dump}"
+  sed -i "s/\`${MATHBOTS_DB_NAME}\`/\`laravel\`/g" "${1:-mathbots.dump}"
+  mariadb -h 127.0.0.1 -P 33061 -ppassword -usail laravel < "${1:-mathbots.dump}"
 }
 
 vldbimportlive() {
