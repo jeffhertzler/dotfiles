e8c259998416a8b9c59496d9d5e836a8f1f72786
Jeff Hertzler <jeffhertzler@gmail.com>
2024-12-16 12:52:03 -0500

    fix completion issue

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -1,6 +1,11 @@
 return {
   "blink.cmp",
   opts = {
+    completion = {
+      trigger = {
+        show_on_insert_on_trigger_character = false,
+      },
+    },
     keymap = {
       ["<C-k>"] = { "select_prev", "fallback" },
       ["<C-j>"] = { "select_next", "fallback" },
