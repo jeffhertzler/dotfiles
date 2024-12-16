741864e5bf22bd6df7a75d1f1a9404e655cc6e57
Jeff Hertzler <jeffhertzler@gmail.com>
2024-12-16 14:43:31 -0500

    disable annoying animation

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -3,6 +3,8 @@ vim.g.loaded_ruby_provider = 0
 
 vim.g.lazyvim_php_lsp = "intelephense"
 
+vim.g.snacks_animate = false
+
 local node22 = vim.fn.trim(vim.fn.system("volta run --node 22 which node"))
 local nodeBin = node22:gsub("/node$", "")
 
