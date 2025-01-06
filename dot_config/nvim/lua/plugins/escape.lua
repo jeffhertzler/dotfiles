400d0a33b1e2fa29776aac58975f2907ecd3ee2b
Jeff Hertzler <jeffhertzler@gmail.com>
2025-01-06 13:52:39 -0500

    don't use better escape in lazygit

--- dot_config/nvim/lua/plugins/escape.lua
+++ dot_config/nvim/lua/plugins/escape.lua
@@ -1,4 +1,17 @@
 return {
   "max397574/better-escape.nvim",
-  opts = {},
+  opts = {
+    mappings = {
+      t = {
+        j = {
+          k = function()
+            if vim.bo.filetype == "snacks_terminal" then
+              return "jk"
+            end
+            return "<esc>"
+          end,
+        },
+      },
+    },
+  },
 }
