e665f652e6fff1b9c28f6797cb6389d9b3332de7
Jeff Hertzler <jeffhertzler@gmail.com>
2025-07-07 16:52:04 -0400

    add more default installed treesitter plugins

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,19 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = {
+      "css",
       "glimmer",
+      "html",
+      "javascript",
+      "latex",
+      "markdown",
+      "markdown_inline",
+      "norg",
+      "scss",
+      "svelte",
+      "tsx",
+      "typst",
+      "vue",
     },
   },
 }
