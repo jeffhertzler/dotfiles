8603e97a712ad045656dded2d30dbf36aecf06ff
Jeff Hertzler <jeffhertzler@gmail.com>
2025-08-18 03:41:28 -0400

    remove telescope aspect of text-case plugin

--- dot_config/nvim/lua/plugins/text-case.lua
+++ dot_config/nvim/lua/plugins/text-case.lua
@@ -1,23 +1,17 @@
 return {
   "johmsalas/text-case.nvim",
-  dependencies = { "nvim-telescope/telescope.nvim" },
   keys = {
     "ga",
-    { "ga.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "x" }, desc = "Telescope" },
   },
   cmd = {
     "S",
     "Subs",
-    "TextCaseOpenTelescope",
-    "TextCaseOpenTelescopeQuickChange",
-    "TextCaseOpenTelescopeLSPChange",
     "TextCaseStartReplacingCommand",
   },
   config = function()
     require("textcase").setup({
       substitude_command_name = "S",
     })
-    require("telescope").load_extension("textcase")
   end,
   lazy = false,
 }
