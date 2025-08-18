e505762967ecc30b65c8a9d0328941a737ad3de0
Jeff Hertzler <jeffhertzler@gmail.com>
2025-08-18 03:41:18 -0400

    remove unused ai plugins

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -1,39 +1,4 @@
 return {
-  -- {
-  --   "sudo-tee/opencode.nvim",
-  --   config = function()
-  --     require("opencode").setup({})
-  --   end,
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     {
-  --       "MeanderingProgrammer/render-markdown.nvim",
-  --       opts = {
-  --         anti_conceal = { enabled = false },
-  --         file_types = { "markdown", "opencode_output" },
-  --       },
-  --       ft = { "markdown", "Avante", "copilot-chat", "opencode_output" },
-  --     },
-  --   },
-  -- },
-  {
-    "Nkr1shna/truffle.nvim",
-    config = function()
-      require("truffle").setup({
-        profiles = {
-          cursor = {
-            command = "cursor-agent",
-            mappings = {
-              toggle = "<leader>at",
-              send_selection = "<leader>as",
-              send_file = "<leader>af",
-              send_input = "<leader>ai",
-            },
-          },
-        },
-      })
-    end,
-  },
   {
     "NickvanDyke/opencode.nvim",
     dependencies = { "folke/snacks.nvim" },
@@ -53,30 +18,4 @@ return {
       { '<S-C-d>',    function() require('opencode').command('messages_half_page_down') end, desc = 'Scroll messages down', },
     },
   },
-  -- {
-  --   "jonroosevelt/gemini-cli.nvim",
-  --   config = function()
-  --     require("gemini").setup()
-  --   end,
-  -- },
-  -- {
-  --   "GeorgesAlkhouri/nvim-aider",
-  --   cmd = "Aider",
-  --   keys = {
-  --     { "<leader>a/", "<cmd>Aider toggle<cr>", desc = "Toggle Aider" },
-  --     { "<leader>as", "<cmd>Aider send<cr>", desc = "Send to Aider", mode = { "n", "v" } },
-  --     { "<leader>ac", "<cmd>Aider command<cr>", desc = "Aider Commands" },
-  --     { "<leader>ab", "<cmd>Aider buffer<cr>", desc = "Send Buffer" },
-  --     { "<leader>a+", "<cmd>Aider add<cr>", desc = "Add File" },
-  --     { "<leader>a-", "<cmd>Aider drop<cr>", desc = "Drop File" },
-  --     { "<leader>ar", "<cmd>Aider add readonly<cr>", desc = "Add Read-Only" },
-  --     { "<leader>aR", "<cmd>Aider reset<cr>", desc = "Reset Session" },
-  --   },
-  --   dependencies = {
-  --     "folke/snacks.nvim",
-  --     --- The below dependencies are optional
-  --     "catppuccin/nvim",
-  --   },
-  --   config = true,
-  -- },
 }
