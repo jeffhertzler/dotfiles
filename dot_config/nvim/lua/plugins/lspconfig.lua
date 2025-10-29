98c327bd81e9f7b8311808399bf7eed1484d8aa5
Jeff Hertzler <jeffhertzler@gmail.com>
2025-10-29 14:01:27 -0400

    refactor how lsp config is handled

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,10 +1,15 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function(_, opts)
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    keys[#keys + 1] = { "<c-k>", false, mode = "i" }
-
-    opts.servers.graphql =
-      { filetypes = { "graphql", "typescriptreact", "javascriptreact", "typescript", "javascript" } }
-  end,
+  opts = {
+    servers = {
+      ["*"] = {
+        keys = {
+          { "<c-k>", false },
+        },
+      },
+      graphql = {
+        filetypes = { "graphql", "typescriptreact", "javascriptreact", "typescript", "javascript" },
+      },
+    },
+  },
 }
