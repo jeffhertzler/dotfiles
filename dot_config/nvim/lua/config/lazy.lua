40a35d4d925aa91c1745baf0b6899e2d7a90fd11
Jeff Hertzler <jeffhertzler@gmail.com>
2026-02-12 10:56:34 -0500

    update theme handling

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -18,7 +18,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "catppuccin" } },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- dot_config/nvim/lua/plugins/theme.lua
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -1,14 +1,23 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  opts = {
-    integrations = {
-      blink_cmp = true,
-      cmp = false,
-      fzf = false,
-      harpoon = true,
-      nvimtree = false,
-      telescope = false,
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      integrations = {
+        blink_cmp = true,
+        cmp = false,
+        fzf = false,
+        harpoon = true,
+        nvimtree = false,
+        telescope = false,
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
     },
   },
 }
