13a48bb471518d26872b176999705a3e1eca6c2d
Jeff Hertzler <jeffhertzler@gmail.com>
2026-02-16 13:14:55 -0500

    higher ram allotment for lsps, consolidate config

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,14 +1,27 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    setup = {
+      eslint = function() end,
+    },
     servers = {
       ["*"] = {
         keys = {
           { "<c-k>", false },
         },
       },
+      eslint = {
+        settings = {
+          execArgv = { "--max-old-space-size=8192" },
+        },
+      },
       graphql = {
         filetypes = { "graphql", "typescriptreact", "javascriptreact", "typescript", "javascript" },
+      },
+      vtsls = {
+        tsserver = {
+          maxTsServerMemory = 8192,
+        },
       },
     },
   },
