3a38acc5b6a22f516d8ff51d2d34c0f1bbd7d30a
Jeff Hertzler <jeffhertzler@gmail.com>
2026-02-28 10:16:40 -0500

    better lsp config

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -10,7 +10,30 @@ return {
           { "<c-k>", false },
         },
       },
+      ember = {
+        root_dir = function(fname)
+          local util = require("lspconfig.util")
+          return util.root_pattern("ember-cli-build.js")(fname)
+        end,
+      },
       eslint = {
+        root_dir = function(fname)
+          local util = require("lspconfig.util")
+          -- If oxlint config is present, disable eslint LSP for this project
+          if util.root_pattern(".oxlintrc.json", "oxlint.config.ts")(fname) then
+            return nil
+          end
+          -- Otherwise use normal eslint root detection
+          return util.root_pattern(
+            ".eslintrc",
+            ".eslintrc.js",
+            ".eslintrc.cjs",
+            ".eslintrc.json",
+            "eslint.config.js",
+            "eslint.config.mjs",
+            "eslint.config.cjs"
+          )(fname)
+        end,
         settings = {
           execArgv = { "--max-old-space-size=8192" },
         },
@@ -18,11 +41,23 @@ return {
       graphql = {
         filetypes = { "graphql", "typescriptreact", "javascriptreact", "typescript", "javascript" },
       },
-      vtsls = {
-        tsserver = {
-          maxTsServerMemory = 8192,
-        },
+      tailwindcss = {
+        root_dir = function(fname)
+          local util = require("lspconfig.util")
+          return util.root_pattern(
+            "tailwind.config.js",
+            "tailwind.config.cjs",
+            "tailwind.config.mjs",
+            "tailwind.config.ts"
+          )(fname)
+        end,
       },
+      vtsls = false,
+      -- vtsls = {
+      --   tsserver = {
+      --     maxTsServerMemory = 8192,
+      --   },
+      -- },
     },
   },
 }
