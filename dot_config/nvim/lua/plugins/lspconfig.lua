552b5fca45c560e46680868cdf41954b77e8ce0d
Jeff Hertzler <jeffhertzler@gmail.com>
2026-03-08 11:50:17 -0400

    fix my root_dir usage

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,9 +1,6 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
-    setup = {
-      eslint = function() end,
-    },
     servers = {
       ["*"] = {
         keys = {
@@ -11,20 +8,22 @@ return {
         },
       },
       ember = {
-        root_dir = function(fname)
+        root_dir = function(bufnr, on_dir)
           local util = require("lspconfig.util")
-          return util.root_pattern("ember-cli-build.js")(fname)
+          local fname = vim.api.nvim_buf_get_name(bufnr)
+          on_dir(util.root_pattern("ember-cli-build.js")(fname))
         end,
       },
       eslint = {
-        root_dir = function(fname)
+        root_dir = function(bufnr, on_dir)
           local util = require("lspconfig.util")
+          local fname = vim.api.nvim_buf_get_name(bufnr)
           -- If oxlint config is present, disable eslint LSP for this project
           if util.root_pattern(".oxlintrc.json", "oxlint.config.ts")(fname) then
-            return nil
+            return
           end
           -- Otherwise use normal eslint root detection
-          return util.root_pattern(
+          local root = util.root_pattern(
             ".eslintrc",
             ".eslintrc.js",
             ".eslintrc.cjs",
@@ -33,6 +32,8 @@ return {
             "eslint.config.mjs",
             "eslint.config.cjs"
           )(fname)
+
+          on_dir(root)
         end,
         settings = {
           execArgv = { "--max-old-space-size=8192" },
@@ -42,14 +43,14 @@ return {
         filetypes = { "graphql", "typescriptreact", "javascriptreact", "typescript", "javascript" },
       },
       tailwindcss = {
-        root_dir = function(fname)
+        root_dir = function(bufnr, on_dir)
           local util = require("lspconfig.util")
-          return util.root_pattern(
-            "tailwind.config.js",
-            "tailwind.config.cjs",
-            "tailwind.config.mjs",
-            "tailwind.config.ts"
-          )(fname)
+          local fname = vim.api.nvim_buf_get_name(bufnr)
+          on_dir(
+            util.root_pattern("tailwind.config.js", "tailwind.config.cjs", "tailwind.config.mjs", "tailwind.config.ts")(
+              fname
+            )
+          )
         end,
       },
       vtsls = false,
