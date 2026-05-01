8ef0de588dd38e9caaabc354b1f740483618775a
Jeff Hertzler <jeffhertzler@gmail.com>
2026-05-01 16:26:06 -0400

    use github releases for neovim

--- dot_linux.zsh
+++ dot_linux.zsh
@@ -1,3 +1,15 @@
-alias nvu="yay -S neovim-nightly --answerclean None --answerdiff None --answeredit None --answerupgrade None --noconfirm"
+#!/usr/bin/env zsh
+
+# alias nvu="yay -S neovim-nightly --answerclean None --answerdiff None --answeredit None --answerupgrade None --noconfirm"
+function nvu() {
+	local tmp
+	tmp="$(mktemp)" || return 1
+	curl -fL "https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage" -o "$tmp" &&
+	mkdir -p ~/.local/bin &&
+	install -m 755 "$tmp" ~/.local/bin/nvim
+	local exit_code=$?
+	rm -f "$tmp"
+	return $exit_code
+}
 alias tu="tmux-preview-update"
 
