a3cc3fc2024c434921e4087eac85dd6b4133590e
Jeff Hertzler <jeffhertzler@gmail.com>
2026-07-17 18:39:04 -0400

    codediff

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -1,28 +1,6 @@
 return {
   {
-    "YouSame2/inlinediff-nvim",
-    cmd = "InlineDiff",
-    keys = {
-      {
-        "<leader>ghi",
-        function()
-          require("inlinediff").toggle()
-        end,
-        desc = "Toggle Inline Diff",
-      },
-    },
-    opts = function()
-      local colors = require("catppuccin.palettes").get_palette()
-      local blend = require("catppuccin.utils.colors").blend
-
-      return {
-        colors = {
-          InlineDiffAddContext = blend(colors.green, colors.base, 0.12),
-          InlineDiffAddChange = blend(colors.green, colors.base, 0.28),
-          InlineDiffDeleteContext = blend(colors.red, colors.base, 0.12),
-          InlineDiffDeleteChange = blend(colors.red, colors.base, 0.28),
-        },
-      }
-    end,
+    "esmuellert/codediff.nvim",
+    cmd = "CodeDiff",
   },
 }
