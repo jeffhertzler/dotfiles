1df33c3ff1aea44b2ade992aea9326c78f867964
Jeff Hertzler <jeffhertzler@gmail.com>
2026-07-27 03:40:32 -0400

    manage profile-specific tunnel helpers

--- dot_mac.zsh
+++ dot_mac.zsh
@@ -4,3 +4,13 @@ alias bubu="brew update; brew upgrade; brew cleanup"
 export K9S_CONFIG_DIR="$HOME/.config/k9s"
 alias k9s='TERM=xterm-ghostty k9s'
 alias ca="agent --yolo"
+
+# Herdr remote entry points for the Arch host. SSH decides whether each host
+# uses the direct, Tailscale, or explicitly remote route.
+function ha() {
+  command herdr --remote arch --remote-keybindings server "$@"
+}
+
+function hat() {
+  command herdr --remote archt --remote-keybindings server "$@"
+}
