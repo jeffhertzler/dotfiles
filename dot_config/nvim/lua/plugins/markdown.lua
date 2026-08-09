af3a2fb682c8d366b4f8abb328e6e2510469a2a5
Jeff Hertzler <jeffhertzler@gmail.com>
2026-08-09 18:11:07 -0400

    configure markdownlint consistently

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -5,10 +5,9 @@ if vim.fn.has("win32") == 1 then
     local app_dir = vim.fs.joinpath(plugin.dir, "app")
     local package_file = vim.fs.joinpath(plugin.dir, "package.json")
     local package = vim.json.decode(table.concat(vim.fn.readfile(package_file), "\n"))
-    local result = vim.system(
-      { "cmd.exe", "/d", "/c", "install.cmd", "v" .. package.version },
-      { cwd = app_dir, text = true }
-    ):wait()
+    local result = vim
+      .system({ "cmd.exe", "/d", "/c", "install.cmd", "v" .. package.version }, { cwd = app_dir, text = true })
+      :wait()
 
     if result.code ~= 0 then
       error(result.stderr ~= "" and result.stderr or result.stdout)
@@ -17,6 +16,33 @@ if vim.fn.has("win32") == 1 then
 end
 
 return {
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters = {
+        ["markdownlint-cli2"] = {
+          args = { "--config", vim.fs.joinpath(vim.fn.expand("~"), ".markdownlint.json"), "-" },
+        },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters = {
+        ["markdownlint-cli2"] = {
+          args = {
+            "--config",
+            vim.fs.joinpath(vim.fn.expand("~"), ".markdownlint.json"),
+            "--fix",
+            "$FILENAME",
+          },
+        },
+      },
+    },
+  },
   {
     "iamcco/markdown-preview.nvim",
     build = markdown_preview_build,
